83fe00426be8bb28dd775ecc65ee6addc3f11764
Eva Pace <eba.pachi@gmail.com>
2024-06-02 18:41:28 -0300

    add operation

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from collections import defaultdict
 from datetime import datetime
 
 class Operation:
-    def __init__(self, amount: int, date: str, account: int):
+    def __init__(self, amount: int, date: str):
         self.amount = amount
         self.date = date
-        self.account = account
 
 class Statement:
     def __init__(self, balance: int, operations: list[Operation]) -> None:
@@ -21,9 +20,10 @@ class Debt:
 class Bank:
     def __init__(self):
         self.balances = defaultdict(int)
+        self.operations = defaultdict(list)
 
-    def add_operation(self, operation: Operation):
-        pass
+    def add_operation(self, account: int, operation: Operation):
+        self.operations[account].append(operation)
 
     def balance(self, account: int) -> int:
         return self.balances[account]
@@ -37,4 +37,7 @@ class Bank:
 bank = Bank()
 assert bank.balance(1) == 0
 assert bank.statements(1, '2023-01-01', '2023-12-31') == {}
+bank.add_operation(1, Operation(100000, '2050-01-01'))
+# operations in the future shouldn't affect current balance
+assert bank.balance(1) == 0
 assert bank.debt_periods(1) == []
